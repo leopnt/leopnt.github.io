f3c7880aec0468011ecb0161b10c28df95ad6bcd
leopnt <leopnt.dev@gmail.com>
2022-03-17 13:51:00 +0100

    added percentage animation

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,9 +3,29 @@ import React, { useEffect, useState } from "react";
 import "../styles/Loading.css";
 
 const Loading = () => {
+  const [remaining, setRemaining] = useState(0);
+
+  useEffect(() => {
+    if (remaining < 99.9) {
+      setTimeout(() => {
+        setRemaining(remaining + 0.1);
+      }, 2);
+    }
+
+    return () => clearTimeout(remaining);
+  });
+
   return (
     <div className="Loading">
-      <p>⚙️ Loading...</p>
+      {remaining < 99.9 ? (
+        <div>
+          Loading...
+          <br />
+          {remaining.toFixed(1)}%
+        </div>
+      ) : (
+        <div>100%</div>
+      )}
     </div>
   );
 };
