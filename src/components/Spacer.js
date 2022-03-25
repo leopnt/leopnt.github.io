f18accebe5e08b5bf373a5af121d9ee3bf7c6799
leopnt <leopnt.dev@gmail.com>
2022-03-25 22:29:24 +0100

    complete revamp using WebGL in a business card effect

--- src/components/Spacer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "../styles/Spacer.css";
-
-const Spacer = ({ height }) => {
-  return <div className="Spacer" style={{ height }}></div>;
-};
-
-export default Spacer;
--- src/config.js
+++
@@ -1,2 +0,0 @@
-const api_url = "https://leopnt-portfolio-api.herokuapp.com/api/v1/";
-export default api_url;
